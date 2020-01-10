132cd13a3be1325bd0dd6faf9ef0e5c50e00c883
kmswlee <wodndl895@kookmin.ac.kr>
2020-01-10 15:49:01 +0900

    main.py modification

--- models/main.py
+++ models/main.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from pointGenerate import getpoint
 
-my_mask=np.asarray(Image.open("/workspace/images/image1.jpg").resize((128,128)))
-my_img=np.asarray(Image.open("/workspace/images/image2.jpg").resize((128,128)))
+my_mask=np.asarray(Image.open("/workspace/images/image1.jpg").resize((224,224)))
+my_img=np.asarray(Image.open("/workspace/images/image2.jpg").resize((224,224)))
 my_mask=my_mask[:,:,0]
 
 points=getpoint(my_mask,1,1,nearest_neighbor=1)
